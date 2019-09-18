6ee66f18093a2a1c02aa2d6e3776a0c254b0aa9d
温文星 <18475536452@163.com>
2019-09-18 16:24:52 +0800

    Spring Boot 日志配置

---
+++ springboot-03-LoggingApplication/src/main/java/com/dragon/SpringBoot03LoggingApplication.java
@@ -0,0 +1,19 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/9/18 15:59
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@SpringBootApplication
+public class SpringBoot03LoggingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot03LoggingApplication.class, args);
+    }
+}
