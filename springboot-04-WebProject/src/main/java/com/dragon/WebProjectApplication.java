7d956aa0e55e60a1b5b38ab84ed81e49751366de
温文星 <18475536452@163.com>
2019-09-19 17:33:06 +0800

    Spring Boot Web开发

---
+++ springboot-04-WebProject/src/main/java/com/dragon/WebProjectApplication.java
@@ -0,0 +1,19 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/9/19 16:10
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@SpringBootApplication
+public class WebProjectApplication {
+    public static void main(String[] args){
+        SpringApplication.run(WebProjectApplication.class);
+    }
+}
---
+++ springboot-04-WebProject/src/main/java/com/dragon/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.dragon.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/9/19 16:27
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/execute")
+    public String execute(Map<String, Object> map) {
+        map.put("name", "DragonWen");
+        // classpath:/templates/success.html
+        return "success";
+    }
+}
